a141c22e88baf31a476ac1478e44c009594e4302
Gabriel Bianchessi <gabriel.bianchessi21@gmail.com>
2023-09-19 16:49:07 -0300

    started kne config

---
+++ db/migrations/20230913205553_create-transactions.ts
@@ -0,0 +1,9 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('transactions', (table) => {
+    table.primary
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {}
