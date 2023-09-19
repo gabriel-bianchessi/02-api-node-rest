add00709d5ef574e602060f7ea6392fc6816b414
Gabriel Bianchessi <gabriel.bianchessi@grupow.com.br>
2023-09-19 17:12:02 -0300

    added fields to transction table

--- db/migrations/20230913205553_create-transactions.ts
+++ db/migrations/20230913205553_create-transactions.ts
@@ -2,8 +2,13 @@ import { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('transactions', (table) => {
-    table.primary
+    table.uuid('id').primary()
+    table.text('title').notNullable()
+    table.decimal('amount', 10, 2).notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now())
   })
 }
 
-export async function down(knex: Knex): Promise<void> {}
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('transactions')
+}
